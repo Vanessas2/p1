dee5a95b1b1e3eb290f4d21a2659f3c1cd5828d7
Vanessas2 <73441540+Vanessas2@users.noreply.github.com>
2021-03-02 16:38:53 -0300

    Add files via upload

---
+++ tarefas de programacao 1.py
@@ -0,0 +1,27 @@
+'''n = input(" Qual o seu nome? " )
+i = int(input("Qual sua idade? " ))
+estadoCivil = input (" Qual o seu estado civil? " )
+g = input (" Qual o seu gênero? " )
+def nome (n):
+    return n
+print (nome(n))
+def idade (i):
+    return i 
+print (idade(i))
+
+def estado_c (estadoCivil):
+    return estadoCivil
+print(estado_c (estadoCivil ))
+def genero(g):
+    return g
+
+
+print ("Seu nome é: ",nome(n))
+print ("Sua idade é: ", idade(i))                    
+print ("Seu estado civil é: ",estado_c(estadoCivil))
+print ("Seu gênero é: ", genero(g))'''
+
+idadeMinuto = input( "Insira sua idade: ")
+def idade_em_minutos (idadeMinuto)
+    return idadeMinuto
+print idade_em_minutos(idadeMinuto)
